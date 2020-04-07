4d616b855d17d9551e174ff142a702d0cf220fdb
boubkerhennouche <60890165+boubkerhennouche@users.noreply.github.com>
2020-04-07 21:47:11 +0100

    Add files via upload

---
+++ TP5/Arretes.cpp
@@ -0,0 +1,36 @@
+#include "Arretes.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+Arretes::Arretes(int numero, int sommet1,int sommet2, int poids)
+{
+    m_numero = numero;
+    m_poids = poids;
+    m_lien.push_back(sommet1);
+    m_lien.push_back(sommet2);
+}
+
+Arretes::~Arretes()
+{
+
+}
+
+void Arretes::afficher()const{
+    std::cout << "Arrete " << Arretes.m_numero << " : " << "lien entre les sommets " << Arretes.m_lien[0]
+    << "\t" << Arretes.m_lien[1] <<"\t de poids \t" << Arretes.m_poids << std::endl;
+}
+
+int Arretes::getLien(int numero)const{
+    return Arretes.m_lien[numero];
+}
+
+int Arretes::getNum() const
+{
+    return Arretes.m_numero;
+}
+
+int Arretes::getPoids()const
+{
+    return Arretes.m_poids;
+}
---
+++ TP5/Graphe.h
@@ -0,0 +1,28 @@
+#ifndef GRAPHE_H
+#define GRAPHE_H
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Sommets.h"
+#include "Arretes.h"
+
+class Graphe {
+private:
+    bool m_estOriente;
+    std::vector<Arretes*> m_arretes;
+    std::vector<Sommet*> m_sommets;
+public:
+    Graphe(std::string cheminFichierGraphe);
+    ~Graphe();
+    void afficher() const;
+    std::vector<int> BFS(int numero_s0) const;
+    std::vector<int> DFS(int numero_s0) const;
+    std::vector<int> Connexe() const;
+    std::vector<int> nombredeChaines ()const;
+    std::vector<std::vector<int>> kruskal () const;
+};
+
+
+#endif // GRAPHE_H
